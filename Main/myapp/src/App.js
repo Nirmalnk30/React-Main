da8cf1e1e82ea6e499ae9d55fda8054e81443815
Nirmalnk30 <114048163+Nirmalnk30@users.noreply.github.com>
2022-12-14 13:35:57 +0530

    Add files via upload

---
+++ Main/myapp/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Main from './Compnents/Main';
+import './Compnents/style.css';
+function App() {
+  return (
+    <div>
+
+      <Main/>
+
+    </div>
+  );
+}
+
+export default App;
